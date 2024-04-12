3d93edbe7bd6f2835656b3bf60269fe7de63659b
Infinite-Null <ankit.kum.sha9933@gmail.com>
2024-04-12 18:54:33 +0530

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <a
+                    target="_blank"  rel="noreferrer"
+                    href="https://github.com/Infinite-Null/NekoFlix/releases/download/v1/NekoFlix.apk"
+                    className="btn btn-custom btn-lg page-scroll"
+                >
+                  Download Now
+                </a>{" "}
+                {/*<div style={{height:"7px"}}/>*/}
+                <a className="btn btn-custom btn-lg page-scroll" style={{
+                  background: "rgb(45,51,90)",
+                  color: "white",
+                  padding: 15,
+                  borderRadius: 10000,
+                  marginLeft:5
+                }} target="_blank"  rel="noreferrer" href={"https://github.com/Infinite-Null/NekoFlix"}>
+                  Contribute Now
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
