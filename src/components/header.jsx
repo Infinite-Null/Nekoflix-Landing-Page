bcf9955aaa038685d3f4f12a6e77ad5ad1f49d3f
Ankit Kumar Shah <ankit.kum.sha9933@gmail.com>
2024-05-03 00:40:00 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,7 @@ export const Header = (props) => {
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
                     target="_blank"  rel="noreferrer"
-                    href="https://github.com/Infinite-Null/NekoFlix/releases/download/v2/Nekoflix_v2.apk"
+                    href="https://github.com/Infinite-Null/NekoFlix/releases/download/v2/NekoFlix.apk"
                     className="btn btn-custom btn-lg page-scroll"
                 >
                   Download Now
